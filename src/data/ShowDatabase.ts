54d23aeb45813a4381c3dd47c5255c46b378b485
NoellyCosta <noellysallazar@gmail.com>
2021-10-25 17:57:39 -0300

    get shows by week day finalizado

--- src/data/ShowDatabase.ts
+++ src/data/ShowDatabase.ts
@@ -1,9 +1,9 @@
+import { NotFoundError } from "../error/NotFoundError";
 import { Show, ShowOutputDTO, WeekDay } from "../model/Show";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class ShowDatabase extends BaseDatabase {
 
-    private static TABLE_NAME = "Table_Shows"
 
     public async createShow(show: Show): Promise<void> {
         await this.getConnection()
@@ -14,7 +14,7 @@ export class ShowDatabase extends BaseDatabase {
                 end_time: show.getEndTime(),
                 week_day: show.getWeekDay()
             })
-            .into(ShowDatabase.TABLE_NAME)
+            .into(this.tableNames.shows)
 
     }
 
@@ -24,12 +24,12 @@ export class ShowDatabase extends BaseDatabase {
         endTime: number
     ): Promise<ShowOutputDTO[]> {
         const shows = await this.getConnection()
-        .select("*")
-        .where("end_time", ">", `${startTime}`)
-        .andWhere("start_time", "<", `${endTime}` )
-        .from(ShowDatabase.TABLE_NAME)
-        
-    
+            .select("*")
+            .where("end_time", ">", `${startTime}`)
+            .andWhere("start_time", "<", `${endTime}`)
+            .from(this.tableNames.shows)
+
+
 
         return shows.map((show: any) => {
             return {
@@ -41,6 +41,34 @@ export class ShowDatabase extends BaseDatabase {
 
             }
         })
+    }
+
+    public async getShowsByWeekDayOrFail(weekDay: WeekDay): Promise<ShowOutputDTO[]> {
+        const shows = await this.getConnection().raw(`
+        SELECT s.id as id,
+        s.id as bandId,
+        s.start_time as startTime,
+        s.end_time as endTime,
+        s.week_day as weekDay,
+        b.music_genre as mainGenre
+        FROM ${this.tableNames.shows} s
+        LEFT JOIN ${this.tableNames.bands} b ON b.id = s.band_id
+        ORDER BY startTIME ASC 
+
+
+`)
+
+        if (!shows.length) {
+            throw new NotFoundError(`Unable to shows found at ${weekDay}`)
+        }
+
+        return shows[0].map((data: any) => ({
+            id: data.id,
+            bandId: data.bandId,
+            startTime: data.startTime,
+            endTime: data.endTime
+
+        }))
     }
 
 }
